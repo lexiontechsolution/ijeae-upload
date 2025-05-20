acb0b7c45d1c972766f6dc2272b1552cdd1d33f9
lexiontechsolution <lexiontechsolution@gmail.com>
2025-05-20 14:12:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Upload from "./Components/Upload";
 import Publications from "./Components/Publications";
 import PDFViewer from "./Components/PDFViewer";
+import UpdatePublication from "./Components/UpdatePublication";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Upload />} />
         <Route path="/publications" element={<Publications />} />
         <Route path="/view-pdf/:id" element={<PDFViewer />} />
+        <Route path="/update-publication/:id" element={<UpdatePublication />} />
       </Routes>
     </Router>
   );
